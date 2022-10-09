905bddd7d2381bcf756709298fff0c8ee4f059d8
Diovane Pacheti <negociospacheti@gmail.com>
2022-10-08 23:24:53 -0300

    :fire: clear file

--- App.tsx
+++ App.tsx
@@ -1,48 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
+import { View } from 'react-native';
+import { Background } from './src/components/Background';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>App eSports</Text>
-
-      <Button title="send"/>
-      <StatusBar style="auto" backgroundColor="#c6c6c6"/>
-    </View>
+    <Background>
+    </Background>
   );
-}
 
-interface ButtonProps{
-  title: string
-}
-
-function Button(props: ButtonProps){
-  return (
-    <TouchableOpacity>
-      <Text style={styles.titlButton}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title:{
-    color:"#c6c6c6",
-    fontSize:26
-  },
-  titlButton:{
-    color:"#c6c6c6",
-    fontSize:26,
-    borderStyle: 'solid',
-    borderColor: "#c6c6c6",
-    
-    // borderStyle: 1 solid #c6c6c6
-  }
-});
+};
